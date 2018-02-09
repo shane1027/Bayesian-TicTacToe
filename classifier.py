7636b47ee9bb8494a5f598bef38203ec0d13e3a0
shane1027 <sryan8@nd.edu>
2018-02-09 17:23:02 -0500

    calculated all probabilites

--- classifier.py
+++ classifier.py
@@ -6,11 +6,65 @@
 # Shane Ryan
 
 
+# function for calculating relevant probabilites given a board position
+def generate_probability(board_position):
+    x=0
+    o=1
+    b=2   # constants for later indexing probability lists
+    probs = {'x':0, 'o':0, 'b':0,
+             'x-given-win':0, 'o-given-win':0, 'b-given-win':0,
+             'x-given-loss':0, 'o-given-loss':0, 'b-given-loss':0}
+
+    # frequency of each state occurring regardless of win / loss
+    # followed by frequency of win occurring regardless of state
+    # and loss ocurring regardless of state
+    state_freqs = list()
+    wins = 0
+    losses = 0
+    for state in training_freqs[board_position][1]['win']:
+        curr_win = training_freqs[board_position][1]['win'][state]
+        curr_loss = training_freqs[board_position][1]['loss'][state]
+        wins += curr_win
+        losses += curr_loss
+        state_freqs.append(curr_win + curr_loss)
+
+    win_freq = wins / (wins + losses)
+    loss_freq = losses / (wins + losses)
+
+    probs['x'] = state_freqs[x] / sum(state_freqs)
+    probs['o'] = state_freqs[o] / sum(state_freqs)
+    probs['b'] = state_freqs[b] / sum(state_freqs)
+
+    probs['x-given-win'] = training_freqs[board_position][1]['win']['x'] \
+            / wins
+    probs['x-given-loss'] = training_freqs[board_position][1]['loss']['x'] \
+            / losses
+    probs['o-given-win'] = training_freqs[board_position][1]['win']['o'] \
+            / wins
+    probs['o-given-loss'] = training_freqs[board_position][1]['loss']['o'] \
+            / losses
+    probs['b-given-win'] = training_freqs[board_position][1]['win']['b'] \
+            / wins
+    probs['b-given-loss'] = training_freqs[board_position][1]['loss']['b'] \
+            / losses
+
+    # basic checks to verify probabilities add up to 1
+    #print(probs)
+    #print(probs['x-given-win'] + probs['o-given-win'] + probs['b-given-win'])
+    #print(probs['x-given-loss'] + probs['o-given-loss'] + probs['b-given-loss'])
+    #print(probs['x'] + probs['o'] + probs['b'])
+    ## i would leave this as a programmatic check, but sometimes rounding sets
+    ## off false positives, i.e. probs. summing to 0.99
+
+    return probs
+
+
 # read in the tic-tac-toe board data and store as lines in a list
 training_data = list()
 with open("tic-tac-toe.data", "r") as training_data_file:
     for line in training_data_file:
         training_data.append(line)
+
 
 # parse training data, separating each game into wins vs losses, removing
 # spaces in data and storing only the nine values corresponding to state of
@@ -24,6 +78,7 @@ for game_board in training_data:
         losing_boards.append(game_board.replace(" ", "")[0:9])
     else:
         raise ValueError("Training data is incomplete", game_board)
+
 
 # verifying that all game boards are sorted
 if len(training_data) != len(winning_boards) + len(losing_boards):
@@ -63,6 +118,7 @@ training_freqs = {'top_left' : [0, {'win':{'x' : 0, 'o' : 0, 'b' : 0},
                                     'loss':{'x' : 0, 'o' : 0, 'b' : 0}}],
                 }
 
+
 # populate winning frequencies
 for game_board in winning_boards:
     for board_position in training_freqs:
@@ -75,10 +131,14 @@ for game_board in losing_boards:
         board_index = training_freqs[board_position][0]
         training_freqs[board_position][1]['loss'][game_board[board_index]] += 1
 
-# calculate probabilities
+# calculate conditional probabilites for each board position
+state_probs = {}
+for position in training_freqs:
+    state_probs[position] = generate_probability(position)
 
-for key in training_freqs:
-    print(training_freqs[key])
-
-
+# add in the overall probability of win vs. loss
+state_probs['win'] = len(winning_boards) / (len(winning_boards) + \
+        len(losing_boards))
+state_probs['loss'] = len(losing_boards) / (len(winning_boards) + \
+        len(losing_boards))
 
